2ba6b7395db27dae510fa114da4d26be5b194f5e
ahmadhuss <ahmadnet600@gmail.com>
2021-05-10 23:38:25 +0500

    feat: Created `AdminAuthenticate` middleware & register in Kernel
    * If the user is not authenticated then redirects back to the admin login form. So we created  `AdminAuthenticate` middleware which helps to protect our dashboard
    
    * We register this Middleware class `AdminAuthenticate` in Kernel with the short name `auth.admin` so we can consume this one easily on our routes.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.admin' => \App\Http\Middleware\AdminAuthenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
