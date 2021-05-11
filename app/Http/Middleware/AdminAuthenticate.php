e5ba4d0431c82f91517ff28de279b3e0921f657a
ahmadhuss <ahmadnet600@gmail.com>
2021-05-11 21:59:11 +0500

    feat: Overriding very important method in AdminAuthenticate middleware
    * If we didn't use this method then the system will user default guard `web` for the `admin` authenticate.

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -6,6 +6,25 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class AdminAuthenticate extends Middleware
 {
+
+    // Override the `authenticate` method from the `Authenticate` class.
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+       if ($this->auth->guard('admin')->check()) {
+           return $this->auth->shouldUse('admin');
+         }
+        $this->unauthenticated($request, ['admin']);
+    }
+
     /**
      * Get the path the admin should be redirected to when they are not authenticated.
      *
