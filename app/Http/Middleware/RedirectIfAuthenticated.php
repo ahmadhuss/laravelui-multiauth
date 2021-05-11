be266e6e0181540e09b60a3832270aed401e38e0
ahmadhuss <ahmadnet600@gmail.com>
2021-05-11 22:50:40 +0500

    refactor: RedirectIfAuthenticated middleware to redirect admin authenticated guard user

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,11 +22,19 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
+
+            // Check if the user is authenticated with the `admin` guard
+            if (Auth::guard('admin')->check()) {
+                return redirect()->route('dashboard');
+            }
+
+            else if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
         }
-
+        // If there is not any guard present in the request. please open the route.
+        // if (Auth::guard($guard)->check()) returns true then it means the user is
+        // authenticated with the guard, then $next($request) will not work.
         return $next($request);
     }
 }
